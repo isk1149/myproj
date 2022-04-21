9762035781fa2241e0fe1d6f578d19c0a5e6bebf
isk1149 <isk1149@naver.com>
2022-04-21 14:04:32 +0900

    add date format

--- myproj/src/main/java/util/DateFormat.java
+++ myproj/src/main/java/util/DateFormat.java
@@ -10,6 +10,11 @@ public class DateFormat {
 		return date.substring(0, 4) + "년 " + date.substring(4, 6) + "월 " + date.substring(6, 8) + "일";
 	}
 	
+	public static String yearMonthDayHourMinuteFormatByKorean(String date) {
+		return date.substring(0, 4) + "년 " + date.substring(4, 6) + "월 " + date.substring(6, 8) + "일 " + 
+				date.substring(8, 10) + "시 " + date.substring(10, 12) + "분 ";
+	}
+	
 	public static String yearMonthDayHourMinuteSecondFormatByKorean(String date) {
 		return date.substring(0, 4) + "년 " + date.substring(4, 6) + "월 " + date.substring(6, 8) + "일 " + 
 				date.substring(8, 10) + "시 " + date.substring(10, 12) + "분 " + date.substring(12, 14) + "초";
